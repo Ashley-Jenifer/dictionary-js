c19595d6bb3f717a49c8b2dfa34f75b9fab2a62e
Ashley <ashleyjeyaseelan@gmail.com>
2024-02-21 14:46:40 +0530

    Updated css

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
+
 var btn = document.getElementById("btn");
-var btn_del = document.getElementById("btn_del")
+var btn_del = document.getElementById("btn_del");
 
 const handleclick = async () => {
-    var input = document.getElementById("input").value.trim(); // Trim to remove leading and trailing spaces
+    var input = document.getElementById("input").value.trim(); 
     var result = document.getElementById("result");
-    var para = document.getElementById("para")
 
-    if(input == "")
-    {
+    if(input == "") {
         alert("Please enter a word to search")
-    }
-    else if (!/^[a-zA-Z]+$/.test(input)) {
+    } else if (!/^[a-zA-Z]+$/.test(input)) {
         alert("Please enter a valid word containing only alphabets.");
         return;
-    }
-    else{
-    try {
-        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${input}`);
-        const data = await response.json();
+    } else {
+        try {
+            const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${input}`);
+            const data = await response.json();
 
-        // Check if the word exists in the dictionary
-        if (data.title && data.title === "No Definitions Found") {
-            para.textContent = "No definitions found for the word.";
-        } else {
-            // Display the definition
-            para.textContent = data[0].meanings[0].definitions[0].definition;
+            
+            if (data.title && data.title === "No Definitions Found") {
+                result.textContent = "No definitions found for the word.";
+            } else {
+               
+                const firstMeaning = data[0].meanings[0];
+                const partOfSpeech = firstMeaning.partOfSpeech;
+                const definition = firstMeaning.definitions[0].definition;
+                result.textContent = `${input}: ${partOfSpeech}
+                 ${definition}`;
+            }
+        } catch (error) {
+            console.error("Error fetching data:", error);
+            result.textContent = "Error fetching data. Please try again later.";
         }
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        para.textContent = "Error fetching data. Please try again later.";
     }
 };
-}
+
 const handledelete = () => {
-    input.value =""
-    para.textContent =""
-}
+    var input = document.getElementById("input");
+    var result = document.getElementById("result");
+    input.value = "";
+    result.textContent = "Result";
+};
+
