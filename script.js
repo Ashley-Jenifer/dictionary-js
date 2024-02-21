a937efa7f973412703fa4ad6c355c47a7e4f0434
Ashley <ashleyjeyaseelan@gmail.com>
2024-02-21 16:17:34 +0530

    uploading

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
 var btn = document.getElementById("btn");
 var btn_del = document.getElementById("btn_del");
 
 const handleclick = async () => {
-    var input = document.getElementById("input").value.trim(); 
+    var input = document.getElementById("input").value.trim();
     var result = document.getElementById("result");
 
     if(input == "") {
@@ -13,20 +12,28 @@ const handleclick = async () => {
         return;
     } else {
         try {
-            const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${input}`);
-            const data = await response.json();
-
             
-            if (data.title && data.title === "No Definitions Found") {
-                result.textContent = "No definitions found for the word.";
-            } else {
+            result.textContent = "Loading...";
+            setTimeout(async () => {
+                try {
+                    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${input}`);
+                    const data = await response.json();
+
                
-                const firstMeaning = data[0].meanings[0];
-                const partOfSpeech = firstMeaning.partOfSpeech;
-                const definition = firstMeaning.definitions[0].definition;
-                result.textContent = `${input}: ${partOfSpeech} -
-                 ${definition}`;
-            }
+                    if (data.title && data.title === "No Definitions Found") {
+                        result.textContent = "No definitions found for the word.";
+                    } else {
+                        
+                        const firstMeaning = data[0].meanings[0];
+                        const partOfSpeech = firstMeaning.partOfSpeech;
+                        const definition = firstMeaning.definitions[0].definition;
+                        result.textContent = `${input}: ${partOfSpeech} - ${definition}`;
+                    }
+                } catch (error) {
+                    console.error("Error fetching data:", error);
+                    result.textContent = "Error fetching data. Please try again later.";
+                }
+            }, 2000); 
         } catch (error) {
             console.error("Error fetching data:", error);
             result.textContent = "Error fetching data. Please try again later.";
@@ -36,8 +43,9 @@ const handleclick = async () => {
 
 const handledelete = () => {
     var input = document.getElementById("input");
-    var result = document.getElementById("result");
+   
     input.value = "";
     result.textContent = "Result";
 };
+
 
